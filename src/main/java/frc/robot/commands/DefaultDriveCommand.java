fb5362eaa754fc190f6e482afa2025b5b859d302
9962082 <9962082@bedfordnhk12.net>
2026-01-21 16:11:04 +0000

    worked on Default drive command

--- src/main/java/frc/robot/commands/DefaultDriveCommand.java
+++ src/main/java/frc/robot/commands/DefaultDriveCommand.java
@@ -4,18 +4,54 @@
 
 package frc.robot.commands;
 
-import frc.robot.subsystems.ExampleSubsystem;
+import java.util.function.DoubleSupplier;
+
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.Subsystem;
 
-
-public final class DefaultDriveCommand {
-  /** Example static factory for an autonomous command. */
-  public static Command exampleAuto(ExampleSubsystem subsystem) {
-    return Commands.sequence(subsystem.exampleMethodCommand(), new ExampleCommand(subsystem));
+/** An example command that uses an example subsystem. */
+public class DefaultDriveCommand extends Command {
+  @SuppressWarnings({"PMD.UnusedPrivateField", "PMD.SingularField"})
+  private final Subsystem tankDrive;
+  private final DoubleSupplier rightSupplier;
+  private final DoubleSupplier forwardSupplier;
+  /**
+   * Creates a new ExampleCommand.
+   *
+   * @param subsystem The subsystem used by this command.
+   */
+  public DefaultDriveCommand(Subsystem tankDrive, DoubleSupplier rightSupplier, DoubleSupplier forwardSupplier) {
+    this.tankDrive = tankDrive;
+    this.rightSupplier = rightSupplier;
+    this.forwardSupplier = forwardSupplier;
+    // Use addRequirements() here to declare subsystem dependencies.
+    addRequirements(tankDrive);
   }
 
-  private Autos() {
-    throw new UnsupportedOperationException("This is a utility class!");
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {}
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    double right = rightSupplier.getAsDouble();
+    double forward = forwardSupplier.getAsDouble();
+
+    tankDrive.drive(forward, right);
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+    tankDrive.drive(0,0);
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
   }
 }
+
